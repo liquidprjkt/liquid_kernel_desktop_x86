c88427107ebae520f1cc73e63f13f55e91d56bb2
Alexandre Frade <kernel@xanmod.org>
2024-09-15 23:03:38 +0000

    sched: Add yield_type sysctl to reduce or disable sched_yield
    Signed-off-by: Alexandre Frade <kernel@xanmod.org>
    Signed-off-by: shygosh <shygosh@proton.me>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -36,6 +36,8 @@ EXPORT_SYMBOL_GPL(sysctl_long_vals);
 
 #if defined(CONFIG_SYSCTL)
 
+extern int sysctl_sched_yield_type;
+
 /* Constants used for minimum and maximum */
 static const int ngroups_max = NGROUPS_MAX;
 static const int cap_last_cap = CAP_LAST_CAP;
@@ -1459,6 +1461,15 @@ int proc_do_static_key(const struct ctl_table *table, int write,
 }
 
 static const struct ctl_table sysctl_subsys_table[] = {
+	{
+		.procname	= "yield_type",
+		.data		= &sysctl_sched_yield_type,
+		.maxlen		= sizeof (int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec,
+		.extra1		= SYSCTL_ZERO,
+		.extra2		= SYSCTL_TWO,
+	},
 #ifdef CONFIG_PROC_SYSCTL
 	{
 		.procname	= "sysctl_writes_strict",
